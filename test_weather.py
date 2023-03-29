2e3d37e4cb8ad608d88b56935def65cce50b6b27
saii_ch_ <127693953+Saiprakash0949@users.noreply.github.com>
2023-03-29 17:16:13 +0200

    Update test_weather.py
    Get weather-valid city function added

--- test_weather.py
+++ test_weather.py
@@ -3,6 +3,19 @@ from main import get_weather, format_output
 
 
 class TestWeatherApp(unittest.TestCase):
+        def test_get_weather_valid_city(self):
+        # Test with a valid city name
+        city = "London"
+        weather_data = get_weather(city)
+        self.assertIsInstance(weather_data, dict)
+        self.assertNotIn('error', weather_data)
+        self.assertIn('location', weather_data)
+        self.assertIn('description', weather_data)
+        self.assertIn('temperature', weather_data)
+        self.assertIn('humidity', weather_data)
+        self.assertIn('wind_speed', weather_data)
+        self.assertIn('sunrise_time', weather_data)
+        self.assertIn('sunset_time', weather_data)
         
     def test_format_output_valid_data(self):
         # Test with valid weather data
