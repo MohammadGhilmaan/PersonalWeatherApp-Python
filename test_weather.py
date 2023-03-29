7787ead4c51ecdc569ac1140f7d3f9e310ac41ac
MohammadGhilmaan <67389203+MohammadGhilmaan@users.noreply.github.com>
2023-03-29 16:11:59 +0200

    Update test_weather.py

--- test_weather.py
+++ test_weather.py
@@ -24,27 +24,6 @@ class TestWeatherApp(unittest.TestCase):
         self.assertIsInstance(weather_data, dict)
         self.assertIn('error', weather_data)
 
-    def test_format_output_valid_data(self):
-        # Test with valid weather data
-        weather_data = {
-            'location': 'London, GB',
-            'description': 'Clear',
-            'temperature': 15.0,
-            'humidity': 77,
-            'wind_speed': 3.09,
-            'sunrise_time': '06:08:38',
-            'sunset_time': '17:48:17'
-        }
-        expected_output = "Location: London, GB\n" \
-                          "Conditions: Clear\n" \
-                          "Temperature: 15.00°C\n" \
-                          "Humidity: 77%\n" \
-                          "Wind Speed: 3.09 m/s\n" \
-                          "Sunrise: 06:08:38\n" \
-                          "Sunset: 17:48:17"
-        output = format_output(weather_data)
-        self.assertEqual(output, expected_output)
-
     def test_format_output_invalid_data(self):
         # Test with invalid weather data (i.e. error message)
         weather_data = {'error': 'Invalid city name'}
